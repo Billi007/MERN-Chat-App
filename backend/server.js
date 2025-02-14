b532de3586b0c2b44467c89d897171e1d9512803
Tanishka <guptatanishka2008@gmail.com>
2025-02-14 13:53:49 +0530

    adding commands for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ import cors from 'cors'
 
 const port = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json()); // to pasrse incoming routes with JSON payload(from req.body)
@@ -21,13 +20,20 @@ app.use(cors({
     credentials: true, // allow cookies to be sent back to the client
     
 }));
-    
+
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
 
-app.use(express.static(path.join(__dirname, '/frontend/dist')))
+if(process.env.NODE_ENV === 'production'){
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '.frontend', 'dist', 'index.html'))
+    })
+    
+}
 
 server.listen(port, () => {
     dbConnect()
