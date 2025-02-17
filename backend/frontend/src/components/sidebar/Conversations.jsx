d76c832d533d51873d1bf3be780ad4a9c59c8320
Tanishka <guptatanishka2008@gmail.com>
2025-02-17 15:35:06 +0530

    add

--- backend/frontend/src/components/sidebar/Conversations.jsx
+++ backend/frontend/src/components/sidebar/Conversations.jsx
@@ -9,7 +9,7 @@ const Conversations = () => {
 
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <h1 className="px-8 py-2 text-slate-300 font-normal bg-slate-900 ">Messages</h1>
+      <h1 className="px-8 py-2 text-slate-300 font-normal bg-slate-900 ">Messagesssss</h1>
       <h1 className="text-white">{authUser.fullname} </h1>
       <div className="py-2 flex-1 overflow-y-auto"
       style={{ maxHeight: "calc(84vh - 10vh)" }}>
